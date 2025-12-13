c6ad377b3e7bc36df9fba74ecf10c2a7c8ba4c53
samarth bhardwaj <bhardwajsamarth122@gmail.com>
2025-12-13 09:15:12 +0530

    EDA and problem statements

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
+
+#def get_requirements(file_path: str) -> List[str]:
+#            │               │            │
+#            │               │            └── returns list of strings
+#            │               └──────────── input must be string
+#            └────────────────────── function name
+
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
